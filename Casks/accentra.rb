66c74763f3ac851813bc3b64e32561ffd4ab6cce
ipavicevic <67849335+ipavicevic@users.noreply.github.com>
2026-07-20 09:42:27 -0700

    accentra 1.0.7

--- Casks/accentra.rb
+++ Casks/accentra.rb
@@ -1,9 +1,9 @@
 cask "accentra" do
   arch arm: "arm64", intel: "x64"
 
-  version "1.0.6"
-  sha256 arm:   "0b2a98aee5e5e19e9b528eacb95faeeaf3d4fc52c7c8cd1d59877cffcaea4807",
-         intel: "207f48e1d18bc7b15662375f1ee838177cef2a38ee30293ff33ff22b752a8a94"
+  version "1.0.7"
+  sha256 arm:   "6af61cfd07b87c4c4347adb45b937ff4a35649fd8cbf39bf7760500b5ca253bb",
+         intel: "b2f85be1ffdc5da7aa21b6d011d45161fb17c8ab7227a8fe0e4b40b8f553983a"
 
   url "https://github.com/ipavicevic/Accentra/releases/download/mac-v#{version}/Accentra-#{version}-#{arch}.dmg"
   name "Accentra"
